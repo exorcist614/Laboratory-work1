a8850464715d6ea441f1262a6ef73f1855375115
exorcist614 <78104718+exorcist614@users.noreply.github.com>
2021-02-01 18:59:04 +0200

    Add files via upload

---
+++ IHT9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+//a-temperature in degrees Celsius
+//b-temperature in fahrenheit
+{
+
+   float a, b;
+   cin>>b;
+   a=(b-32)*5/9;
+   cout<<"(b-32)*5/9="<<a;
+
+	return 0;
+}
